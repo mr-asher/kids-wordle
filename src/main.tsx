f50b860a2def5a835354e4973fc9e305cbc72f6e
Asher <jasherleslie@gmail.com>
2025-02-26 10:21:22 +1100

    add back analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "react-simple-keyboard/build/css/index.css";
 import Wordling from "./Wordling";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Wordling />
+    <Analytics />
   </StrictMode>,
 );
