85f0d9699fd45ba08bae0dba697f0153f55b476b
Asher <jasherleslie@gmail.com>
2025-02-26 10:19:36 +1100

    Remove analytics. Will fix later.

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,9 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "react-simple-keyboard/build/css/index.css";
 import Wordling from "./Wordling";
-import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Wordling />
-    <Analytics />
   </StrictMode>,
 );
